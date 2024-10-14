ba44c8abb30ff558ab95f68f73abf5c526b1d069
Stephan Kostin <va.lenta0907@gmail.com>
2024-10-14 18:00:12 +0500

    Update UI

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import { Formik } from 'formik';
 
@@ -32,28 +33,40 @@ function Login() {
           handleBlur,
           handleSubmit,
           isSubmitting,
-          /* and other goodies */
         }) => (
           <form onSubmit={handleSubmit}>
-            <input
-              type="email"
-              name="email"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.email}
-            />
-            {errors.email && touched.email && errors.email}
-            <input
-              type="password"
-              name="password"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.password}
-            />
-            {errors.password && touched.password && errors.password}
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
+            <div className="mb-3">
+              <label htmlFor="a" className="form-label">Email address</label>
+              <input
+                type="email"
+                name="email"
+                className="form-control"
+                id="a"
+                aria-describedby="emailHelp"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                values={values.email}
+              />
+              <div id="emailHelp" className="">{errors.email && touched.email && errors.email}</div>
+            </div>
+            <div>
+              <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+              <input
+                type="password"
+                name="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+              />
+              <div id="emailHelp" className="">{errors.password && touched.password && errors.password}</div>
+            </div>
+            <div className="mb-3 from-check">
+              <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+              <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" className="btn btn-primary" onSubmit={isSubmitting}>Submit</button>
           </form>
         )}
       </Formik>
