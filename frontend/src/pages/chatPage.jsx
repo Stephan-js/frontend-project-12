5b746eb9e82a3d5b196a59c60160dce3e1a1e043
Stephan Kostin <va.lenta0907@gmail.com>
2024-10-31 02:32:03 +0500

    Dev Update

--- frontend/src/pages/chatPage.jsx
+++ frontend/src/pages/chatPage.jsx
@@ -31,13 +31,13 @@ function ChatPage() {
       </nav>
       <div className="h-100 container-fluid my-4 my-md-5 d-flex">
         <div className="container h-100 overflow-hidden align-self-center chat rounded-4 shadow">
-          <div className="row h-100 bg-white flex-md-row">
-            <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
+          <div className="d-flex h-100 bg-white flex-column row flex-md-row">
+            <div className="col col-md-2 border-bottom border-md-end px-0 bg-light flex-row flex-md-column h-100 d-flex">
               <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
                 <b>Chaneles</b>
                 <button type="button" className="p-0 text-primary btn btn-group-vertical">+</button>
               </div>
-              <ul className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
+              <ul className="d-felx flex-row flex-md-column px-2 mb-3 nav-pills overflow-auto h-100 d-block">
                 {channels ? channels.map((chan) => <Channel {...chan} />) : null}
                 <Channel name="first" removable />
                 <Channel name="second" removable active />
