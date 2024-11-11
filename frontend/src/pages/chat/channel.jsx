9b77d9a233922c056b51d13f2c52e8db6a50eed3
Stephan Kostin <va.lenta0907@gmail.com>
2024-11-11 11:57:32 +0500

    Update

--- frontend/src/pages/chat/channel.jsx
+++ frontend/src/pages/chat/channel.jsx
@@ -1,33 +1,24 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+
 import React from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 
 class Channel extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.state = { show: false };
 
     this.handleClick = this.handleClick.bind(this);
-    this.handleClickMenu = this.handleClickMenu.bind(this);
     this.handleDeleteChannel = this.handleDeleteChannel.bind(this);
     this.handleRenameChannel = this.handleRenameChannel.bind(this);
-    this.handleBlur = this.handleBlur.bind(this);
   }
 
   handleClick(e) {
     const { setActive } = this.props;
     const { id } = e.target;
     if (id) setActive(id);
-  }
-
-  handleClickMenu() {
-    this.setState(({ show }) => ({ show: !show }));
-  }
-
-  handleBlur() {
-    this.setState({ show: false });
   }
 
   handleDeleteChannel(e) {
@@ -54,64 +45,49 @@ class Channel extends React.PureComponent {
       removable,
       activeChannel,
     } = this.props;
-    const active = activeChannel === id;
 
-    if (!removable) {
+    const variant = activeChannel === id ? 'secondary' : 'none';
+    const tooSmall = window.innerWidth < 768;
+    if (!removable || tooSmall) {
       return (
         <div className="nav-item w-md-100 me-1 me-md-0 mb-md-1" style={{ 'min-width': 'fit-content' }}>
-          <button
+          <Button
             id={id}
+            variant={variant}
             onClick={this.handleClick}
-            className={classNames('w-100', 'rounded-3', 'text-start', 'btn', { 'btn-secondary': active })}
+            className="rounded-3 w-100 text-start"
             type="button"
           >
             <span># </span>
             {name}
-          </button>
+          </Button>
         </div>
       );
     }
-    const { show } = this.state;
 
     return (
       <div className="nav-item w-md-100 me-1 me-md-0 mb-md-1" style={{ 'min-width': 'fit-content' }}>
-        <div className="d-flex btn-group">
-          <button
+        <Dropdown as={ButtonGroup} className="w-100">
+          <Button
             id={id}
+            variant={variant}
             onClick={this.handleClick}
-            className={classNames(
-              'w-100',
-              'rounded-3',
-              'rounded-end-0',
-              'text-start',
-              'btn',
-              { 'btn-secondary': active },
-            )}
+            className="rounded-3 w-100 text-start rounded-end-0"
             type="button"
           >
             <span># </span>
             {name}
-          </button>
-          <button
-            onClick={this.handleClickMenu}
-            // onBlur={this.handleBlur}
-            className={classNames(
-              'flex-grow-0',
-              'rounded-3',
-              'rounded-start-0',
-              'btn',
-              'dropdown-toggle-split',
-              'dropdown-toggle',
-              { show, 'btn-secondary': active },
-            )}
-            type="button"
-            data-bs-toggle="dropdown"
-            aria-expanded={show}
+          </Button>
+          <Dropdown.Toggle
+            split
+            variant={variant}
+            className="rounded-3 rounded-start-0"
+            id="dropdown-split-basic"
           >
             <span className="visually-hidden">Toggle Dropdown</span>
-          </button>
-          <ul
-            className={classNames('dropdown-menu', 'rounded-3', { show })}
+          </Dropdown.Toggle>
+          <Dropdown.Menu
+            className="rounded-3"
             style={{
               position: 'absolute',
               'min-width': '0px',
@@ -120,10 +96,10 @@ class Channel extends React.PureComponent {
               transform: 'translate(0px, 40px)',
             }}
           >
-            <li><a id={id} className="dropdown-item" onClick={this.handleRenameChannel} href="#">Rename</a></li>
-            <li><a id={id} className="dropdown-item" onClick={this.handleDeleteChannel} href="#">Delete</a></li>
-          </ul>
-        </div>
+            <Dropdown.Item id={id} onClick={this.handleRenameChannel} href="#">Rename</Dropdown.Item>
+            <Dropdown.Item id={id} onClick={this.handleDeleteChannel} href="#">Delete</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
       </div>
     );
   }
