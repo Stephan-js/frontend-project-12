7db082912f930c5f40f97f782fb0f25976af675d
Stephan Kostin <va.lenta0907@gmail.com>
2024-11-28 07:14:09 +0500

    Fix

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,36 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider,
   Navigate,
-} from 'react-router-dom';
+} from "react-router-dom";
 
-import './scss/style.scss';
-import 'react-toastify/dist/ReactToastify.css';
+import "./scss/style.scss";
+import "react-toastify/dist/ReactToastify.css";
 
-import Login from './pages/login';
-import Page404 from './pages/page404';
-import Register from './pages/register';
-import ChatPage from './pages/chatPage';
+import Login from "./pages/login";
+import Page404 from "./pages/page404";
+import Register from "./pages/register";
+import ChatPage from "./pages/chatPage";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: localStorage.getItem('token') ? <ChatPage /> : <Navigate to="/login" />,
+    path: "/",
+    element: localStorage.getItem("token") ? (
+      <ChatPage />
+    ) : (
+      <Navigate to="/login" />
+    ),
     errorElement: <Page404 />,
   },
   {
-    path: '/login',
-    element: localStorage.getItem('token') ? <Navigate to="/" /> : <Login />,
+    path: "/login",
+    element: localStorage.getItem("token") ? <Navigate to="/" /> : <Login />,
   },
   {
-    path: '/register',
-    element: localStorage.getItem('token') ? <Navigate to="/" /> : <Register />,
+    path: "/register",
+    element: localStorage.getItem("token") ? <Navigate to="/" /> : <Register />,
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
